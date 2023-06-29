31d83fb0a61d88fe7521228261eb73c1c315bdb9
Atsuki02 <canvanvam@gmail.com>
2023-06-28 17:03:29 -0700

    first commit

---
+++ src/pages/PageNotFount.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageNav from "../components/PageNav";
+
+function PageNotFount() {
+  return (
+    <div>
+      <PageNav /> Not found
+    </div>
+  );
+}
+
+export default PageNotFount;
